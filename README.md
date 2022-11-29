a5334171b8d846051d071523a77dd9a21c9a54c2
Sergio de Mingo <sdemingo@gmail.com>
2022-11-29 07:56:14 +0100

    update README

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# journal
-Simple app in rust to manage my journals
+# Journal
+
+Journal is a simple app to manage my journals and life logs files. From years
+I've been using a simple format to store what happened in a day. 
+
+
+## My journal files
+
+With this command I can extract information from this logs file. This app
+expect your journals directory as:
+
+```
+journal_dir
+     |
+     |------ 2022
+     |       |------ 2022-01.md
+     |       |------ 2022-02.md
+     |       |------ 2022-03.md
+     |       |------ one file per month
+     |
+     |------ 2021
+     |------ 2020
+     |------ one dir per year
+
+```
+
+A month file looks like:
+
+```
+# March
+
+## Monday - March 1st
+
+Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis
+facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta
+vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.
+Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis
+varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit
+hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.
+Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie
+mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed
+id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a
+sapien.
+
+tag1: Aenean in sem ac leo mollis blandit.
+tag2: Donec neque quam, dignissim in, mollis nec, sagittis eu, wisi.  
+
+## Tuesday - March 2nd
+
+....
+
+```
+
+The first one line paragraph is a summary day. Next, I use tags (work, coding,
+friends, etc.) to remark special events about something.
+
+
+
+## Usage
+
+You can use `journal` with nexts options:
+
+* `-d dd/mm/yy`: Show the entry for this day.
+* `-n N`: Show the nexts `N` entries from `-d` option.
+
+Options are not implement yet:
+
+* `-m month`: Show entries only for this month of the current year.
+* `-y year`: Show entries only for this year.
+* `-t tag`: Show the tags entries in current month. You can combine with `-m` or `-y`.
+* `-p pattern`: Show entries that contains the pattern. You can combine with `-m` or `-y`.
+
--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,140 @@
-
-
-use std::fs;
-use std::env;
-use std::process;
 use std::cmp::Ordering;
+use std::env;
+use std::fs;
+use std::process;
 
-use chrono::*;
 use argparse::*;
+use chrono::*;
 
-
-struct Entry{
-    date:NaiveDate,
-    text:String,
+struct Entry {
+    date: NaiveDate,
+    text: String,
 }
-
 
 const JOURNAL_PATH: &str = "/notebook/diarios/2022";
 
-
 // Parsed and whole entry from an text fragment
-fn parse_entries(e_vector: &mut Vec<Entry>, file_path: &String){
-
-    let contents = fs::read_to_string(file_path)
-        .expect("ERROR: No se encuentra fichero indicado");
+fn parse_entries(e_vector: &mut Vec<Entry>, file_path: &String) {
+    let contents = fs::read_to_string(file_path).expect("ERROR: No se encuentra fichero indicado");
     let entries_text = contents.split("## ");
-    for e in entries_text{
+    for e in entries_text {
         let entry_title = e.split("\n").nth(0).unwrap();
-        let edate=parse_entry_date(entry_title.to_string(), &file_path);
+        let edate = parse_entry_date(entry_title.to_string(), &file_path);
         match edate {
             Ok(date) => {
-                let entry = Entry{date: date, text:e.to_string()};
+                let entry = Entry {
+                    date: date,
+                    text: e.to_string(),
+                };
                 e_vector.push(entry);
-            },
-            _ => {},
-        }        
+            }
+            _ => {}
+        }
     }
 }
 
 // Parsed an entry date from the entry title. It's first line
-fn parse_entry_date(entry_title:String, file_path: &String)  -> ParseResult<NaiveDate>{
-    
-    let day: String = entry_title.chars()
+fn parse_entry_date(entry_title: String, file_path: &String) -> ParseResult<NaiveDate> {
+    let day: String = entry_title
+        .chars()
         .filter(|entry_title| entry_title.is_digit(10))
         .collect();
 
-    let file_path_cur = file_path.replace(".md","");
+    let file_path_cur = file_path.replace(".md", "");
     let fields = file_path_cur.split("/");
     let mut filename = fields.last().unwrap().split("-");
     let year = filename.next().unwrap();
     let month = filename.next().unwrap();
 
-    let sdate = format!("{}/{}/{}",day,month,year);
-    return NaiveDate::parse_from_str(&sdate,"%d/%m/%Y");
+    let sdate = format!("{}/{}/{}", day, month, year);
+    return NaiveDate::parse_from_str(&sdate, "%d/%m/%Y");
 }
 
-
-
 // Parsed an string date from user
-fn parse_date(sdate:String) -> NaiveDate{
-    let edate=NaiveDate::parse_from_str(&sdate,"%d/%m/%y");
+fn parse_date(sdate: String) -> NaiveDate {
+    let edate = NaiveDate::parse_from_str(&sdate, "%d/%m/%y");
     return match edate {
         Ok(date) => date,
         Err(_err) => Utc::now().naive_utc().date(),
-    };        
+    };
 }
 
-
-
-
 // Get a entry by a date
-fn get_entry_by_date(e_vector: &Vec<Entry>, date:NaiveDate) -> Option<&Entry>{
-    for e in e_vector{
-        if e.date.cmp(&date) == Ordering::Equal{
+fn get_entry_by_date(e_vector: &Vec<Entry>, date: NaiveDate) -> Option<&Entry> {
+    for e in e_vector {
+        if e.date.cmp(&date) == Ordering::Equal {
             return Some(e);
         }
     }
     return None;
 }
 
-
 // Print an entry for a date
-fn print_entry_by_date(e_vector: &Vec<Entry>, date:NaiveDate){
+fn print_entry_by_date(e_vector: &Vec<Entry>, date: NaiveDate) {
     let entry = get_entry_by_date(e_vector, date);
     match entry {
-        Some(ent) =>     {
-            println!("{}",date.format("\x1b[1m == [%d/%m/%y] ==\x1b[0m"));
-            let lines=textwrap::wrap(&ent.text,80);
-            for l in lines{
-                println!("{}",l);
+        Some(ent) => {
+            println!("{}", date.format("\x1b[1m == [%d/%m/%y] ==\x1b[0m"));
+            let lines = textwrap::wrap(&ent.text, 80);
+            for l in lines {
+                println!("{}", l);
             }
-        },
-        _=>{},
+        }
+        _ => {}
     }
 }
 
-
-fn main(){
-
+fn main() {
     let mut date = "".to_string();
     let mut next = 0;
 
-    {  // argparse block code
+    {
+        // argparse block code
         let mut ap = ArgumentParser::new();
         ap.set_description("Aplicación para la gestión de diarios");
         ap.refer(&mut date)
-            .add_option(&["-d", "--date"], Store,
-                        "Mostrar entradas de una fecha");
+            .add_option(&["-d", "--date"], Store, "Mostrar entradas de una fecha");
 
-        ap.refer(&mut next)
-            .add_option(&["-n", "--next"], Store,
-                        "Mostrar las siguientes entradas sobre la fecha");
+        ap.refer(&mut next).add_option(
+            &["-n", "--next"],
+            Store,
+            "Mostrar las siguientes entradas sobre la fecha",
+        );
 
         let args: Vec<_> = env::args().collect();
         if args.len() == 1 {
             println!("Se necesitan opciones. Use -h para obtener ayuda");
             process::exit(0);
-        }else{
+        } else {
             ap.parse_args_or_exit();
-            
         }
     }
 
     let mut path = env::var("HOME").expect("$HOME is not set");
     path.push_str(JOURNAL_PATH);
 
-    let mut entries_vector : Vec<Entry> = Vec::new();
+    let mut entries_vector: Vec<Entry> = Vec::new();
 
     let paths = fs::read_dir(path).unwrap();
     for path in paths {
-        parse_entries(&mut entries_vector, &path.unwrap().path().display().to_string());
+        parse_entries(
+            &mut entries_vector,
+            &path.unwrap().path().display().to_string(),
+        );
     }
 
-    entries_vector.sort_by(|a,b| b.date.cmp(&a.date));
+    entries_vector.sort_by(|a, b| b.date.cmp(&a.date));
 
-    if date!=""{ 
+    if date != "" {
         let real_date = parse_date(date);
         print_entry_by_date(&entries_vector, real_date);
 
-        if next>0{
-            for i in 0..next{
+        if next > 0 {
+            for i in 0..next {
                 let next_date = real_date.checked_add_days(Days::new(i));
-                match next_date{
-                    Some(nxt)=> print_entry_by_date(&entries_vector,nxt),
-                    _=>{},
+                match next_date {
+                    Some(nxt) => print_entry_by_date(&entries_vector, nxt),
+                    _ => {}
                 }
             }
         }
